69483095d7390f73f3061c7fb8ede4c98ef479b0
007steve <stephen.plummer1.sp@gmail.com>
2020-11-30 16:04:09 -0500

    finish hedaer

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,24 @@
-import React from 'react'
-import "../styles/Header.css"
+import React from "react";
+import "../styles/Header.css";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import SettingsIcon from "@material-ui/icons/Settings";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import SearchIcon from '@material-ui/icons/Search';
 function Header() {
-    return (
-        <div>
-            header
-        </div>
-    )
+  return (
+    <div className="header">
+      <form action="">
+          <SearchIcon/>
+        <input placeholder="Search" />
+      </form>
+
+      <div className="header-icons">
+        <NotificationsIcon className="header-icon" />
+        <SettingsIcon className="header-icon" />
+        <ExitToAppIcon className="header-icon" />
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;

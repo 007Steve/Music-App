da9e176dc0adae6345dd6a9f939ddb53932694f2
007steve <stephen.plummer1.sp@gmail.com>
2020-12-03 01:24:40 -0500

    added ccard with redux slice

---
+++ src/components/Card.js
@@ -0,0 +1,37 @@
+import React, { useEffect } from "react";
+import "../styles/Card.css";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+
+import { useDispatch } from "react-redux";
+import { setCurrentSong } from "../features/currentSongSlice";
+function Card({ album, artist, time, image, song, id }) {
+  const dispatch = useDispatch();
+  // const [currentSong, setCurrentSong] = useState([]);
+  const songOne = song;
+  useEffect(() => {
+    selectedSongHandler();
+  }, []);
+  const selectedSongHandler = () => {
+    setCurrentSong(songOne);
+    dispatch(
+      setCurrentSong({
+        setCurrentSong: song,
+      })
+    );
+    console.log(song);
+  };
+
+  return (
+    <div className="card" onClick={selectedSongHandler}>
+      <img className="card-image" src={song.cover} alt="" />
+      <div className="card-container">
+        <h4 className="card-album">{song.song}</h4>
+        <p className="card-artist">{song.artist}</p>
+        <p className="card-time">{time}</p>
+        <FavoriteIcon className="card-like" />
+      </div>
+    </div>
+  );
+}
+
+export default Card;

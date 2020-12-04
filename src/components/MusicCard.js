d9fbfd9b761c6edc94a56e6dcb4e4af49d72e730
007steve <stephen.plummer1.sp@gmail.com>
2020-12-03 20:27:35 -0500

    added music card

---
+++ src/components/MusicCard.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "../styles/MusicCard.css";
+import { useDispatch } from "react-redux";
+import { setCurrentSong } from "../features/currentSongSlice";
+function MusicCard({ image, album, artist, song }) {
+  const dispatch = useDispatch();
+  const selectedSongHandler = () => {
+    dispatch(
+      setCurrentSong({
+        setCurrentSong: song,
+      })
+    );
+    console.log(song);
+  };
+  return (
+    <div className="musicCard" onClick={selectedSongHandler}>
+      <img className="musicCard-image" src={image} alt="" />
+      <div className="musicCard-info-container">
+        <h3 className="musicCard-info-album">{album}</h3>
+        <p className="musicCard-info-artist">{artist}</p>
+      </div>
+    </div>
+  );
+}
+
+export default MusicCard;

88776bea6b4d06717c318fd60f31e2b3f227b4ff
007steve <stephen.plummer1.sp@gmail.com>
2020-12-28 23:12:59 -0500

    added components and data

--- src/App.js
+++ src/App.js
@@ -3,22 +3,68 @@ import logo from "./logo.svg";
 import { Counter } from "./features/counter/Counter";
 import "./App.css";
 import Sidebar from "./components/Sidebar";
-import Header from "./components/Header";
-import Row from "./components/Row";
+import Row from "./components/Row.js";
 import MainContainer from "./components/MainContainer";
-
+import { selectUser } from "./features/userSlice";
+import { selectcurrentSong } from "./features/currentSongSlice";
+import { useSelector } from "react-redux";
+import { songs } from "./features/songsSlice";
+import { useDispatch } from "react-redux";
+import SongsData from "./SongsData";
+import Login from "./components/Login";
 
 function App() {
+
   
+
+
+
+  // USER
+  const user = useSelector(selectUser);
+//DISPATCH
+  const dispatch = useDispatch();
+
+  const [songz, setSongz] = useState(SongsData());
+  console.log(songz);
+
+  useEffect(() => {
+    songss();
+    // selectedSongHandler()
+  }, []);
+  // SONG
+  const songss = () => {
+    dispatch(
+      songs({
+        songs: songz,
+      })
+    );
+  };
+
+  // const selectedSongHandler = () => {
+  //   setCurrentSong(songOne);
+  //   dispatch(
+  //     setCurrentSong({
+  //       setCurrentSong: song,
+  //     })
+  //   );
+  
+  // };
   return (
-    <div className="App">
-      <Sidebar />
-      <div className="container">
-      
-        <Row />
-        <MainContainer />
-      </div>
-    </div>
+    <>
+
+      {user ? (
+        <div className="App">
+          <Sidebar />
+         
+          <div className="container">
+            <Row />
+            <MainContainer />
+          </div>
+        </div>
+      ) : (
+        <Login />
+      )}
+    </>
   );
 }
 

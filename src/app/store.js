0bf32bd918f97e009ba831a109f9b152887fd533
007steve <stephen.plummer1.sp@gmail.com>
2020-12-29 00:24:11 -0500

    updated

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
 import songsReducer from "../features/songsSlice";
 import userReducer from "../features/userSlice";
 import currentSongReducer from "../features/currentSongSlice";
 export default configureStore({
   reducer: {
-    counter: counterReducer,
+    
     songs: songsReducer,
     user: userReducer,
     currentSong: currentSongReducer,

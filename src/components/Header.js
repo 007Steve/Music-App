af8b171f54a78ba142081aaeb7946c41f57aa475
007steve <stephen.plummer1.sp@gmail.com>
2020-12-28 23:23:40 -0500

    add redux logout

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,18 +4,29 @@ import NotificationsIcon from "@material-ui/icons/Notifications";
 import SettingsIcon from "@material-ui/icons/Settings";
 import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 import SearchIcon from "@material-ui/icons/Search";
+import { useDispatch } from "react-redux";
+import { logout} from "../features/userSlice";
 function Header() {
+  const dispatch = useDispatch()
+  const setLogoutHandler = (e) => {
+     e.preventDefault()
+     dispatch(logout({
+      
+       user:null
+     }))
+     
+  }
   return (
     <div className="header">
-      <form action="">
+      <form className="header-form" action="">
         <SearchIcon />
-        <input placeholder="Search" />
+        <input className="header-input"placeholder="Search" />
       </form>
 
       <div className="header-icons">
         <NotificationsIcon className="header-icon" />
         <SettingsIcon className="header-icon" />
-        <ExitToAppIcon className="header-icon" />
+        <ExitToAppIcon className="header-icon" onClick={setLogoutHandler} />
       </div>
     </div>
   );

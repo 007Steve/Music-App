97cab7f7b0f931871f06be50eb30e1ebe0bcabd2
007steve <stephen.plummer1.sp@gmail.com>
2020-12-29 22:38:46 -0500

    add song slice

--- src/components/Row.js
+++ src/components/Row.js
@@ -3,20 +3,21 @@ import "../styles/Row.css";
 import MusicCard from "./MusicCard";
 import songs from "../SongsData";
 import Header from "./Header";
-
+import { selectSongs } from "../features/songsSlice";
+import { useSelector } from "react-redux";
 function Row() {
   const [song, setSong] = useState(songs());
-
+  const songe = useSelector(selectSongs);
   return (
     <div className="row">
       <Header />
       <h2 className="row-title">Trending</h2>
       <div className="row-container">
-        {song.map((song) => (
+        {songe.map((song) => (
           <MusicCard
             image={song.cover}
             artist={song.artist}
-            album={song.song}
+            album={song.title}
             key={song.id}
             song={song}
           />

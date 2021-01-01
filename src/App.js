be6a7272b06318d0c1c1827e56ec0c837011ab99
007steve <stephen.plummer1.sp@gmail.com>
2021-01-01 13:48:09 -0500

    removed usestate and cleaned up

--- src/App.js
+++ src/App.js
@@ -1,42 +1,40 @@
-import React, { useState, useEffect } from "react";
-
+import React, { useEffect } from "react";
 import "./App.css";
 import Sidebar from "./components/Sidebar";
 import Row from "./components/Row.js";
 import MainContainer from "./components/MainContainer";
 import { selectUser } from "./features/userSlice";
-import { selectcurrentSong,setCurrentSong } from "./features/currentSongSlice";
+import { setCurrentSong } from "./features/currentSongSlice";
 import { useSelector } from "react-redux";
-import { songs , selectSongs} from "./features/songsSlice";
+import { songs, selectSongs } from "./features/songsSlice";
 import { useDispatch } from "react-redux";
 import Login from "./components/Login";
 
 function App() {
- // USER
+  // USER
   const user = useSelector(selectUser);
   const currentSongS = useSelector(selectSongs);
 
-//DISPATCH
+  //DISPATCH
   const dispatch = useDispatch();
- console.log(currentSongS)
-  useEffect(() => {
-   fetch("http://localhost:3000/songs")
-   .then(response => response.json())
-   .then(data => dispatch(songs(data)))
-   dispatch(
-    setCurrentSong({
-      setCurrentSong:currentSongS[0],
-     } )
-  );
-  }, [])
 
+  useEffect(() => {
+    fetch("http://localhost:3000/songs")
+      .then((response) => response.json())
+      .then((data) => dispatch(songs(data)));
+  }, []);
+  //DISPATCH CURRENT SONG
+  dispatch(
+    setCurrentSong({
+      setCurrentSong: currentSongS[0],
+    })
+  );
   return (
     <>
-
       {user ? (
         <div className="App">
           <Sidebar />
-         
+
           <div className="container">
             <Row />
             <MainContainer />

20286457cdea4ad5d72d85cec03960ca0da77faa
007steve <stephen.plummer1.sp@gmail.com>
2021-01-04 16:47:18 -0500

    updated format

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,22 +5,22 @@ import SettingsIcon from "@material-ui/icons/Settings";
 import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 import SearchIcon from "@material-ui/icons/Search";
 import { useDispatch } from "react-redux";
-import { logout} from "../features/userSlice";
+import { logout } from "../features/userSlice";
 function Header() {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const setLogoutHandler = (e) => {
-     e.preventDefault()
-     dispatch(logout({
-      
-       user:null
-     }))
-     
-  }
+    e.preventDefault();
+    dispatch(
+      logout({
+        user: null,
+      })
+    );
+  };
   return (
     <div className="header">
       <form className="header-form" action="">
         <SearchIcon />
-        <input className="header-input"placeholder="Search" />
+        <input className="header-input" placeholder="Search" />
       </form>
 
       <div className="header-icons">
